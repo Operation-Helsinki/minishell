bcb17af173fa9e3726a0a68d9134f2b224908090
David Garcia lou <davgarci@c1r11s5.42madrid.com>
2023-02-04 21:45:25 +0100

    punto_hache

--- inc/builtins.h
+++ inc/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psegura- <psegura-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davgarci <davgarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 21:03:40 by psegura-          #+#    #+#             */
-/*   Updated: 2023/02/01 18:44:53 by psegura-         ###   ########.fr       */
+/*   Updated: 2023/02/04 09:54:53 by davgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,11 @@ int		ft_export(char **env, char **args);
 
 /*__PWD_____*/
 int		ft_pwd(void);
+
+/*__SIGNALS___*/
+void	sig_init(void);
+void	execution_signal_init(void);
+void	execution_signal_hadler(int sig);
 
 /*__UNSET___*/
 int		ft_unset(char **env, char **args);
