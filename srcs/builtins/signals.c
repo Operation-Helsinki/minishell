b7edbab15ab4842b1a9edfe1b0a97a21ba667809
psegura- <psegura-@student.42madrid.com>
2023-02-16 09:16:45 +0100

    Signals and execution fixed

--- srcs/builtins/signals.c
+++ srcs/builtins/signals.c
@@ -6,61 +6,11 @@
 /*   By: psegura- <psegura-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 23:37:32 by psegura-          #+#    #+#             */
-/*   Updated: 2023/02/16 09:11:55 by psegura-         ###   ########.fr       */
+/*   Updated: 2023/02/16 09:16:32 by psegura-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// void	handle_sig(int sig)
-// {
-// 	if (sig == SIGINT)
-// 	{
-// 		g_c.dolar_q = 130;
-// 		write(1, "\n", 1);
-// 		rl_on_new_line();
-// 		rl_replace_line("", 0);
-// 		rl_redisplay();
-// 	}
-// 	else if (sig == SIGQUIT)
-// 	{
-// 		write(2, "\b\b  \b\b", 6);
-// 		// printf("ESTO es Ctrl + /\n");
-// 		// rl_redisplay();
-// 		return ;
-// 	}
-// }
-
-// void	sig_init(void)
-// {
-// 	if (signal(SIGINT, handle_sig) == SIG_ERR)
-// 		exit(EXIT_FAILURE);
-// 	else if (signal(SIGQUIT, handle_sig) == SIG_ERR)
-// 		exit(EXIT_FAILURE);
-// }
-
-// void	execution_signal_hadler(int sig)
-// {
-// 	if (sig == SIGINT)
-// 	{
-// 		free(g_c.tokens);
-// 		g_c.dolar_q = 130;
-// 		write(2, "\n", 1);
-// 	}
-// 	else if (sig == SIGQUIT)
-// 	{
-// 		g_c.dolar_q = 131;
-// 		ft_putstr_fd("Exit, bye\n", 2);
-// 	}
-// }
-
-// void	execution_signal_init(void)
-// {
-// 	if (signal(SIGINT, execution_signal_hadler) == SIG_ERR)
-// 		exit(EXIT_FAILURE);
-// 	else if (signal(SIGQUIT, execution_signal_hadler) == SIG_ERR)
-// 		exit(EXIT_FAILURE);
-// }
 
 void	signal_parent(int signal)
 {
